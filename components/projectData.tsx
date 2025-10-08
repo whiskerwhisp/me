f3685ae2b7f95b951eb8a8847335f4548433a578
imriyarana <riyarana1437@gmail.com>
2025-10-08 08:39:54 +0530

    new project added

--- components/projectData.tsx
+++ components/projectData.tsx
@@ -1,14 +1,15 @@
 import { House, IceCreamBowl, Key } from "lucide-react";
 
 export const ProjectData = [
+  
   {
-    title: "CasaCozy",
-    text: "A cozy rental platform where users can discover unique stays, create their own listings, and manage them with full CRUD functionality.",
-    icon: <House />,
-    bg: "bg-[#de4a55]/30",
-    tech: ["Next.js", "MongoDB", "Tailwind"],
-    githubUrl: "https://github.com/whiskerwhisp",
-    liveUrl: "https://casacozy.vercel.app",
+    title: "Locksy",
+    text: " A strong password generator with a built-in secure vault to store and manage your passwords safely.",
+    icon: <Key />,
+    bg: "bg-[#a9a9a9]/60",
+    tech: ["Next.js", "MongoDB", "TS"],
+    githubUrl: "https://github.com/whiskerwhisp/Locksy",
+    liveUrl: "https://locksy-vault.vercel.app/",
   },
   {
     title: "Frozio",
@@ -20,13 +21,13 @@ export const ProjectData = [
     liveUrl: "https://frozio.vercel.app/",
   },
   {
-    title: "Locksy",
-    text: " A strong password generator with a built-in secure vault.",
-    icon: <Key />,
-    bg: "bg-[#a9a9a9]/60",
-    tech: ["Next.js", "MongoDB", "TS"],
+    title: "CasaCozy",
+    text: "A cozy rental platform where users can discover unique stays, create their own listings, and manage them with full CRUD functionality.",
+    icon: <House />,
+    bg: "bg-[#de4a55]/30",
+    tech: ["Next.js", "MongoDB", "Tailwind"],
     githubUrl: "https://github.com/whiskerwhisp",
-    liveUrl: "https://locksy-demo.vercel.app",
+    liveUrl: "https://casacozy.vercel.app",
   },
 ];
 
